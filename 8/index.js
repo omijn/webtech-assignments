85a8fd4056672b76d74d473330a103130f33aed3
omijn <abhijit.kashyap07@gmail.com>
2018-04-09 00:33:01 -0700

    search form

---
+++ 8/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+
+app.listen(3000, () => console.log('App listening on port 3000'))
