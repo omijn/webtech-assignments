c575f4d816be48a77357610b5c1f0d7b4e7cafb5
omijn <abhijit.kashyap07@gmail.com>
2018-04-11 01:55:45 -0400

    return nearby search results

--- 8/index.js
+++ 8/index.js
@@ -16,24 +16,24 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.post('/nearby', jsonParser, (req, res) => {
+app.post('/nearby', jsonParser, (req, res, next) => {
 	
-	var keyword = req.body.keyword
-	var category = req.body.category
-	var distance = req.body.distance * 1609.34
+	res.locals.keyword = req.body.keyword
+	res.locals.category = req.body.category
+	res.locals.distance = req.body.distance * 1609.34
 	var location_type = req.body.location_type
-	var loc = req.body.loc
+	res.locals.loc = req.body.loc
 
 	if (location_type == "address") {
 		googleMapsClient.geocode({
-			address: loc
+			address: res.locals.loc
 		}, (err, response) => {
 
 			if (!err) {
 				var lat = response.json.results[0].geometry.location.lat
 				var lng = response.json.results[0].geometry.location.lng
-				var loc = lat + "," + lng
-				nearbySearch(keyword, category, distance, loc)
+				res.locals.loc = lat + "," + lng
+				next()
 			}
 			else
 				console.log(err)
@@ -41,25 +41,35 @@ app.post('/nearby', jsonParser, (req, res) => {
 	}
 
 	else {
-		nearbySearch(keyword, category, distance, loc)
+		next()
 	}
-
-	res.send("reached /nearby route successfully")
-})
-
-function nearbySearch(keyword, type, radius, location) {
+	
+}, (req, res) => {
 	googleMapsClient.placesNearby({
-		keyword: keyword,
-		type: type,
-		radius: radius,
-		location: location
+		keyword: res.locals.keyword,
+		type: res.locals.category,
+		radius: res.locals.distance,
+		location: res.locals.loc
 	}, (err, response) => {
 		if (!err) {
-			console.log(response.json)
+			// console.log(response.json)
+			nearby_response = {}
+			nearby_response.next_page_token = response.json.next_page_token
+			nearby_response.results = []
+			for (let r of response.json.results) {
+				single_result = {}
+				single_result.place_id = r.place_id
+				single_result.icon = r.icon
+				single_result.address = r.vicinity
+				single_result.coords = r.geometry.location.lat + "," + r.geometry.location.lng
+				nearby_response.results.push(single_result)
+			}
+			console.log(nearby_response)
+			res.send(JSON.stringify(nearby_response))
 		}
 		else
 			console.log(err)
 	})
-}
+})
 
 app.listen(2999, () => console.log('App listening on port 2999'))
